e757de91466b824e95a4179d980213c273836b17
NHKritDat <dat01102003@gmail.com>
2023-11-17 15:44:17 +0700

    DBConnect and Paging

--- src/main/java/fpt/fu/prj301_se17c02_undeee/services/DBConnect.java
+++ src/main/java/fpt/fu/prj301_se17c02_undeee/services/DBConnect.java
@@ -4,10 +4,35 @@
  */
 package fpt.fu.prj301_se17c02_undeee.services;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  *
  * @author Admin
  */
 public class DBConnect {
-    
+
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/undeee";
+    private static final String DB_USERNAME = "root";
+    private static final String DB_PASSWORD = "123";
+    public Connection connection = null;
+
+    public DBConnect() {
+        this.connection = getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
+    }
+
+    private Connection getConnection(String url, String username, String password) {
+        Connection c = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            c = DriverManager.getConnection(url, username, password);
+            System.out.println("Connect to Database success!!!");
+        } catch (ClassNotFoundException | SQLException e) {
+            System.out.println("Can't connect to Database!!! " + e.getMessage());
+        }
+        return c;
+    }
+
 }
