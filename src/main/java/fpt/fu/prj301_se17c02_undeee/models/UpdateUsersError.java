44ebcc965b458d01d6b3c8849eff735e846f8b47
phongthuong12 <90915750+phongthuong12@users.noreply.github.com>
2023-12-05 09:21:56 +0700

    update khuya 3/12

--- src/main/java/fpt/fu/prj301_se17c02_undeee/models/UpdateUsersError.java
+++ src/main/java/fpt/fu/prj301_se17c02_undeee/models/UpdateUsersError.java
@@ -11,6 +11,7 @@ package fpt.fu.prj301_se17c02_undeee.models;
 public class UpdateUsersError {
 
     private String updatePhoneError;
+    private String updatePasswordError;
 
     public UpdateUsersError() {
     }
@@ -27,6 +28,20 @@ public class UpdateUsersError {
      */
     public void setUpdatePhoneError(String updatePhoneError) {
         this.updatePhoneError = updatePhoneError;
+    }
+
+    /**
+     * @return the updatePasswordError
+     */
+    public String getUpdatePasswordError() {
+        return updatePasswordError;
+    }
+
+    /**
+     * @param updatePasswordError the updatePasswordError to set
+     */
+    public void setUpdatePasswordError(String updatePasswordError) {
+        this.updatePasswordError = updatePasswordError;
     }
 
 }
